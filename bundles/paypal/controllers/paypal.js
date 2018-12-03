0b92abed3775dadd5714fe16acabde2d572d368e
EdenCoder <alex@adlist.co.nz>
2018-12-03 16:28:25 +1000

    add subscription logic to paypal payment

--- bundles/paypal/controllers/paypal.js
+++ bundles/paypal/controllers/paypal.js
@@ -137,9 +137,30 @@ class PaypalController extends Controller {
     // check method
     if (payment.get('error') || payment.get('method.type') !== 'paypal') return;
 
+    // check type
+    let subscription = await payment.get('subscription');
+
+    // check if subscription
+    if (subscription) {
+      // return normal subscription
+      return await this._subscription(payment, subscription);
+    } else {
+      // return normal payment
+      return await this._normal(payment);
+    }
+  }
+
+  /**
+   * creates normal payment
+   *
+   * @param  {Payment}  payment
+   *
+   * @return {Promise}
+   */
+  async _normal (payment) {
     // load user
-    let invoice = await payment.get('invoice');
     let order   = await payment.get('order');
+    let invoice = await payment.get('invoice');
 
     // let items
     let items = await Promise.all(invoice.get('lines').map(async (line) => {
@@ -223,7 +244,7 @@ class PaypalController extends Controller {
     // create paypal redirect url
     return await new Promise((resolve, reject) => {
       // create payment
-      paypal.payment.create(payReq, (e, payment) => {
+      paypal.payment.create(payReq, (e, paypalPayment) => {
         // get links
         let links = {};
 
@@ -237,7 +258,7 @@ class PaypalController extends Controller {
         }
 
         // Capture HATEOAS links
-        payment.links.forEach((linkObj) => {
+        paypalPayment.links.forEach((linkObj) => {
           // set link to object
           links[linkObj.rel] = {
             'href'   : linkObj.href,
@@ -252,7 +273,222 @@ class PaypalController extends Controller {
             'redirect' : links['approval_url'].href
           });
           payment.set('paypal', {
-            'id' : payment.id
+            'id' : paypalPayment.id
+          });
+
+          // set redirect
+          return resolve(payment.set('redirect', payment.get('data.redirect')));
+        } else {
+          return resolve(payment.set('error', {
+            'id'   : 'paypal.nourl',
+            'text' : 'no redirect URI present'
+          }));
+        }
+      });
+    });
+  }
+
+  /**
+   * create payment subscription
+   *
+   * @param  {Payment}      payment
+   * @param  {Subscription} subscription
+   *
+   * @return {Promise}
+   */
+  async _subscription (payment, subscription) {
+    // load user
+    let order   = await payment.get('order');
+    let invoice = await payment.get('invoice');
+
+    // let items
+    let items = await Promise.all(invoice.get('lines').map(async (line) => {
+      // priced
+      let item = {
+        'qty'     : line.qty,
+        'opts'    : line.opts || {},
+        'user'    : await order.get('user'),
+        'product' : await product.findById(line.product)
+      };
+
+      // return price
+      await this.eden.hook('product.order', item);
+
+      // let opts
+      let opts = {
+        'qty'   : parseInt(line.qty),
+        'item'  : item,
+        'base'  : (parseFloat(item.price) || 0),
+        'price' : (parseFloat(item.price) || 0) * parseInt(line.qty),
+        'order' : order
+      };
+
+      // price item
+      await this.eden.hook('line.price', opts);
+
+      // return object
+      return {
+        'sku'      : item.product.get('sku').replace('ALI-', '') + (Object.values(line.opts || [])).join('_'),
+        'name'     : item.product.get('title.en-us'),
+        'type'     : item.product.get('type'),
+        'price'    : money.floatToAmount(opts.base),
+        'currency' : payment.get('currency') || 'USD',
+        'quantity' : opts.qty,
+      };
+    }));
+
+    // get all subscription items
+    let subscriptionItems = items.filter((item) => {
+      // check if subscription
+      return item.type === 'subscription';
+    });
+
+    // get all normal items
+    let normalItems = items.filter((item) => {
+      // check if subscription
+      return item.type !== 'subscription';
+    });
+
+    // get real total
+    let normalDisc  = '0';
+    let normalTotal = normalItems.reduce((accum, line) => {
+      // return accum
+      return money.add(accum, money.floatToAmount(parseFloat(line.price) * (line.quantity || 1)));
+    }, '0');
+
+    // set periods
+    let periods = {
+      'weekly' : {
+        'frequency'          : 'WEEK',
+        'frequency_interval' : '1'
+      },
+      'monthly' : {
+        'frequency'          : 'MONTH',
+        'frequency_interval' : '1'
+      },
+      'quarterly' : {
+        'frequency'          : 'MONTH',
+        'frequency_interval' : '3'
+      },
+      'biannually' : {
+        'frequency'          : 'MONTH',
+        'frequency_interval' : '6'
+      },
+      'annually' : {
+        'frequency'          : 'YEAR',
+        'frequency_interval' : '1'
+      }
+    };
+
+    // get total
+    let billingPlan = {
+      'name'        : 'Subscription plan for #' + payment.get('_id').toString(),
+      'type'        : 'INFINITE',
+      'description' : 'Subscription plan for #' + payment.get('_id').toString(),
+      'payment_definitions' : [
+        {
+          'name'               : 'Subscription #' + subscription.get('_id').toString(),
+          'type'               : 'REGULAR',
+          'frequency'          : periods[subscription.get('period')].frequency,
+          'frequency_interval' : periods[subscription.get('period')].frequency_interval,
+          'amount' : {
+            'value'    : subscription.get('price').toFixed(2),
+            'currency' : 'USD'
+          },
+          'cycles' : '0'
+        }
+      ],
+      'merchant_preferences' : {
+        'setup_fee' : {
+          'value'    : normalTotal,
+          'currency' : 'USD'
+        },
+        'return_url'                 : 'https://' + config.get('domain') + '/paypal/process',
+        'cancel_url'                 : 'https://' + config.get('domain') + '/paypal/cancel',
+        'auto_bill_amount'           : 'YES',
+        'max_fail_attempts'          : '0',
+        'initial_fail_amount_action' : 'CONTINUE'
+      }
+    };
+
+    // create billing plan
+    let createdPlan = await new Promise((resolve, reject) => {
+      // create plan
+      paypal.billingPlan.create(billingPlan, (e, plan) => {
+        // reject error
+        if (e) return reject(e);
+
+        // resolve plan
+        resolve(plan);
+      });
+    });
+
+    // Activate the plan by changing status to Active
+    await new Promise((resolve, reject) => {
+      // update created plan
+      paypal.billingPlan.update(createdPlan.id, [
+        {
+          'op'    : 'replace',
+          'path'  : '/',
+          'value' : {
+            'state' : 'ACTIVE'
+          }
+        }
+      ], (e, res) => {
+        // resolve
+        if (e) return reject(e);
+
+        // resolve
+        resolve(res);
+      });
+    });
+
+    // create actual agreement
+    let billingAgreement = {
+      'name'        : 'Payment for invoice #' + invoice.get('_id').toString() + '.',
+      'start_date'  : ((new Date()).toISOString().slice(0, 19) + 'Z'),
+      'description' : 'Payment for invoice #' + invoice.get('_id').toString() + '.',
+      'plan' : {
+        'id' : createdPlan.id
+      },
+      'payer' : {
+        'payment_method' : 'paypal'
+      }
+    };
+
+    // create paypal redirect url
+    return await new Promise((resolve, reject) => {
+      // create payment
+      paypal.billingAgreement.create(billingAgreement, (e, billingAgreement) => {
+        // get links
+        let links = {};
+
+        // check error
+        if (e) {
+          // set error
+          return resolve(payment.set('error', {
+            'id'   : 'paypal.error',
+            'text' : e.toString()
+          }));
+        }
+
+        // Capture HATEOAS links
+        billingAgreement.links.forEach((linkObj) => {
+          // set link to object
+          links[linkObj.rel] = {
+            'href'   : linkObj.href,
+            'method' : linkObj.method
+          };
+        });
+
+        // If redirect url present, redirect user
+        if (links.hasOwnProperty('approval_url')) {
+          // set date
+          payment.set('data', {
+            'redirect' : links['approval_url'].href
+          });
+          payment.set('paypal', {
+            'id' : billingAgreement.id
           });
 
           // set redirect
